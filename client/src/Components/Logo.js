50e73ab6fc60821b3d5241b3c7959634f1437ee1
ZeGrandmaster <60135314+danikrisdayadi@users.noreply.github.com>
2021-10-09 23:39:54 +0800

    add href to buttons

--- client/src/Components/Logo.js
+++ client/src/Components/Logo.js
@@ -8,7 +8,7 @@ function Logo() {
         <AppBar position="static" style={{ background: '#FFF' }}>
             <Container maxWidth="xxl">
             <Toolbar>
-                <IconButton edge="start" sx={{ ml: 20, my: 1, "&:hover": { backgroundColor: "transparent" } }}  disableRipple>
+                <IconButton edge="start" sx={{ ml: 20, my: 1, "&:hover": { backgroundColor: "transparent" } }} href="/" disableRipple>
                     <Box component="img" 
                             sx={{
                                 height: '4rem'
@@ -18,8 +18,8 @@ function Logo() {
                 </IconButton>
                 <Box sx={{flex: 1}} />
                 <Box sx={{mr: 20}}>
-                    <Button sx={{width: 100, fontWeight: 500, mr: 3, textTransform: 'none'}} variant="outlined" disableElevation>Login</Button>
-                    <Button sx={{width: 100, textTransform: 'none' }} variant="contained" disableElevation>Sign Up</Button>
+                    <Button sx={{width: 100, fontWeight: 500, mr: 3, textTransform: 'none'}} variant="outlined" disableElevation href="/login">Login</Button>
+                    <Button sx={{width: 100, textTransform: 'none' }} variant="contained" disableElevation href="/signup">Sign Up</Button>
                 </Box>
             </Toolbar>
             </Container>
