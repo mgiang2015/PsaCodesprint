f3e546b46565f860c5399aa304894da8de6a3e90
frisciliasultan <frisciliasultan@gmail.com>
2021-10-09 21:34:13 +0800

    Add request

--- server/controllers/requestController.js
+++ server/controllers/requestController.js
@@ -1,92 +1,100 @@
-const express = require('express');
+import express from 'express';
+import { Request } from '../models/request';
 
-const Request = require('../models/request');
-
-const requestController = {
-    getAllRequests(req, res) {
-        try {
-            Request.find({}).then((requests) => {
+export async function getAllRequests(req, res) {
+    try {
+        Request.find({})
+            .populate('origin destination operator')
+            .then((requests) => {
                 res.statusCode = 200;
                 res.setHeader('Content-Type', 'application/json');
                 res.json(requests);
             });
-        } catch (err) {
-            res.status(500).send(err);
-        }
-    },
+    } catch (err) {
+        res.status(500).send(err);
+    }
+}
 
-    getRequest(req, res, next) {
-        Request.findById(req.params.requestId)
-            .then((request) => {
-                if (request != null) {
-                    res.statusCode = 200;
-                    res.setHeader('Content-Type', 'application/json');
-                    res.json(request);
-                } else {
-                    let err = new Error(
-                        'Request ' + req.params.requestId + ' not found!'
-                    );
-                    res.statusCode = 404;
-                    return next(err);
-                }
-            })
-            .catch((err) => next(err));
-    },
-
-    postRequest(req, res) {
-        try {
-            // Creates new Request with the updated req.body
-            const newRequest = new Requests(req.body);
-
-            // Save Request to DB
-            const request = await newRequest.save();
-
-            const populatedRequests = await(Requests).findById(request._id);
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'application/json');
-            res.json(populatedRequests);
-        } catch (err) {
-            if (err.errors) {
-                res.statusCode = 400;
-                res.send(err);
+export async function getRequest(req, res, next) {
+    Request.findById(req.params.requestId)
+        .populate('origin destination operator')
+        .then((request) => {
+            if (request != null) {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(request);
             } else {
-                res.statusCode = 500;
-                res.send(err);
+                let err = new Error(
+                    'Request ' + req.params.requestId + ' not found!'
+                );
+                res.statusCode = 404;
+                return next(err);
             }
-        }
-    },
+        })
+        .catch((err) => next(err));
+}
 
-    updateRequest(req, res) {
-        const request = await Requests.findByIdAndUpdate(
-            req.params.requestId,
-            {
-                $set: req.body
-            },
-            { new: true }
-        );
+export async function postRequest(req, res) {
+    try {
+        // Creates new Request with the updated req.body
+        const newRequest = new Request(req.body);
+
+        // Save Request to DB
+        const request = await newRequest.save();
 
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
         res.json(request);
-    },
-
-    deleteRequest(req, res) {
-        const request = await Requests.findById(req.params.requestId);
-        if (request != null) {
-            const removedRequest = await Requests.findByIdAndRemove(
-                req.params.requestId
-            );
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'application/json');
-            res.json(removedRequest);
+    } catch (err) {
+        if (err.errors) {
+            res.statusCode = 400;
+            res.send(err);
         } else {
-            let err = new Error(
-                'Request ' + req.params.requestId + ' not found!'
-            );
-            res.statusCode = 404;
-            return next(err);
+            res.statusCode = 500;
+            res.send(err);
         }
     }
-};
+}
 
-module.exports = requestController;
+export async function updateRequest(req, res) {
+    const request = await Request.findByIdAndUpdate(
+        req.params.requestId,
+        {
+            $set: req.body
+        },
+        { new: true }
+    );
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json(request);
+}
+
+export async function deleteRequest(req, res) {
+    const request = await Request.findById(req.params.requestId);
+    if (request != null) {
+        const removedRequest = await Request.findByIdAndRemove(
+            req.params.requestId
+        );
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(removedRequest);
+    } else {
+        let err = new Error('Request ' + req.params.requestId + ' not found!');
+        res.statusCode = 404;
+        return next(err);
+    }
+}
+
+export async function deleteAllRequests(req, res) {
+    try {
+        const resp = await Request.deleteMany({});
+
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(resp);
+    } catch (err) {
+        res.statusCode = 500;
+        res.send(err);
+    }
+}
