ecd13d184f4331363d6abacb6ef1826fbd7563e1
ZeGrandmaster <60135314+danikrisdayadi@users.noreply.github.com>
2021-10-10 03:36:50 +0800

    add sign up page

--- client/src/UserAuth/SignupPage.js
+++ client/src/UserAuth/SignupPage.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { useForm } from "react-hook-form";
-import { Box, Paper, TextField, Button } from '@mui/material';
-
+import { Paper, TextField, Button, Container, Box, Divider } from '@mui/material';
+import { Logo } from "../assets/exportLogo";
+import { PaddingY } from "../Utils/Padding";
 
 export default function SignupPage() {
 	const { register, handleSubmit } = useForm();
@@ -11,19 +12,35 @@ export default function SignupPage() {
 	}
 
 	return (
-	<Box>
-		<form onSubmit={handleSubmit(onSubmit)}>
-			<Paper sx={{ display: 'flex',
-										flexDirection: 'column',
-										alignItems: 'center',
-										width: 1/4 }}>
-				<TextField label="Username" variant="outlined" {...register("Username")} />
-				<TextField label="Password" variant="outlined" type="password"  {...register("Password")}/>
-				<TextField label="Confirm Password" variant="outlined" type="password" {...register("Confirm")}/>
-				<Button>Create Account</Button>
-			</Paper>
-		</form>
-		<Button>Login to Your Account</Button>
-	</Box>
+        <Container sx={{height: "80vh", width: "100%", alignItems:"center", justifyContent: "center", display: "flex"}}>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <Paper elevation={12}
+                    sx={{ display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        px: 6,
+                        py: 6,
+                        borderRadius: 2
+                    }}>
+                    <Logo height={100}/>
+                    <h1>Create New Account</h1>
+                    <Box sx={{flexDirection: 'column',display: 'flex', gap: 2, width: "40vh"}}>
+                        <TextField label="Username" variant="outlined" {...register("Username")} />
+                        <TextField label="Password" variant="outlined" type="password"  {...register("Password")}/>
+                        <TextField label="Confirm Password" variant="outlined" type="password" {...register("Confirm")}/>
+                    </Box>
+                    <PaddingY padding={1.5} />
+                    <Box sx={{display: 'flex', justifyContent: 'flex-end', width: "100%"}}>
+                        <Button size="large" variant="contained" sx={{display: 'flex', px: 5, textTransform: 'none'}}>Sign Up</Button>
+                    </Box>
+                    <PaddingY padding={2} />
+
+                    <Divider style={{width:'100%'}}>or</Divider>
+
+                    <PaddingY padding={2} />
+                    <Button variant="contained" sx={{display: 'flex', px: 5, textTransform: 'none', width: '100%'}} href="/login">Login</Button>
+                </Paper>
+            </form>
+        </Container>
 	)
 }
