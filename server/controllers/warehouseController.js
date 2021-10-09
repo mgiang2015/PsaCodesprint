933f7c6b84c915aa80561f7532576e335dcabe91
frisciliasultan <frisciliasultan@gmail.com>
2021-10-09 20:01:24 +0800

    Remove comment and implement warehouse

--- server/controllers/warehouseController.js
+++ server/controllers/warehouseController.js
@@ -1,103 +1,99 @@
-const express = require('express');
+import express from 'express';
+import { Warehouse } from '../models/warehouse';
 
-const Warehouse = require('../models/warehouse');
-
-const warehouseController = {
-    getAllWarehouses(req, res) {
-        try {
-            Warehouse.find({}).then((warehouses) => {
-                res.statusCode = 200;
-                res.setHeader('Content-Type', 'application/json');
-                res.json(warehouses);
-            });
-        } catch (err) {
-            res.status(500).send(err);
-        }
-    },
-
-    getWarehouse(req, res, next) {
-        Warehouse.findById(req.params.warehouseId)
-            .then((warehouse) => {
-                if (warehouse != null) {
-                    res.statusCode = 200;
-                    res.setHeader('Content-Type', 'application/json');
-                    res.json(warehouse);
-                } else {
-                    let err = new Error(
-                        'Warehouse ' + req.params.warehouseId + ' not found!'
-                    );
-                    res.statusCode = 404;
-                    return next(err);
-                }
-            })
-            .catch((err) => next(err));
-    },
-
-    postWarehouse(req, res) {
-        try {
-            // Creates new Warehouse with the updated req.body
-            const newWarehouse = new Warehouse(req.body);
-
-            // Save Warehouse to DB
-            const warehouse = await newWarehouse.save();
-
+export async function getAllWarehouses(req, res) {
+    try {
+        Warehouse.find({}).then((warehouses) => {
             res.statusCode = 200;
             res.setHeader('Content-Type', 'application/json');
-            res.json(warehouse);
-        } catch (err) {
-            if (err.errors) {
-                res.statusCode = 400;
-                res.send(err);
-            } else {
-                res.statusCode = 500;
-                res.send(err);
-            }
-        }
-    },
+            res.json(warehouses);
+        });
+    } catch (err) {
+        res.status(500).send(err);
+    }
+}
 
-    updateWarehouse(req, res) {
-        const warehouse = await Warehouse.findByIdAndUpdate(
-            req.params.warehouseId,
-            {
-                $set: req.body
-            },
-            { new: true }
-        );
+export async function getWarehouse(req, res, next) {
+    Warehouse.findById(req.params.warehouseId)
+        .then((warehouse) => {
+            if (warehouse != null) {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(warehouse);
+            } else {
+                let err = new Error(
+                    'Warehouse ' + req.params.warehouseId + ' not found!'
+                );
+                res.statusCode = 404;
+                return next(err);
+            }
+        })
+        .catch((err) => next(err));
+}
+
+export async function postWarehouse(req, res) {
+    try {
+        // Creates new Warehouse with the updated req.body
+        const newWarehouse = new Warehouse(req.body);
+
+        // Save Warehouse to DB
+        const warehouse = await newWarehouse.save();
 
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
         res.json(warehouse);
-    },
-
-    deleteWarehouse(req, res) {
-        const warehouse = await Warehouse.findById(req.params.warehouseId);
-        if (warehouse != null) {
-            const removedWarehouse = await Warehouses.findByIdAndRemove(
-                req.params.warehouseId
-            );
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'application/json');
-            res.json(removedWarehouse);
+    } catch (err) {
+        if (err.errors) {
+            res.statusCode = 400;
+            res.send(err);
         } else {
-            let err = new Error(
-                'Warehouse ' + req.params.warehouseId + ' not found!'
-            );
-            res.statusCode = 404;
-            return next(err);
-        }
-    },
-    deleteAllWarehouses(req, res) {
-        try {
-            const resp = await Warehouse.deleteMany({});
-
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'application/json');
-            res.json(resp);
-        } catch (err) {
             res.statusCode = 500;
             res.send(err);
         }
     }
-};
+}
 
-module.exports = warehouseController;
+export async function updateWarehouse(req, res) {
+    const warehouse = await Warehouse.findByIdAndUpdate(
+        req.params.warehouseId,
+        {
+            $set: req.body
+        },
+        { new: true }
+    );
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json(warehouse);
+}
+
+export async function deleteWarehouse(req, res) {
+    const warehouse = await Warehouse.findById(req.params.warehouseId);
+    if (warehouse != null) {
+        const removedWarehouse = await Warehouse.findByIdAndRemove(
+            req.params.warehouseId
+        );
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(removedWarehouse);
+    } else {
+        let err = new Error(
+            'Warehouse ' + req.params.warehouseId + ' not found!'
+        );
+        res.statusCode = 404;
+        return next(err);
+    }
+}
+
+export async function deleteAllWarehouses(req, res) {
+    try {
+        const resp = await Warehouse.deleteMany({});
+
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(resp);
+    } catch (err) {
+        res.statusCode = 500;
+        res.send(err);
+    }
+}
---
+++ server/routes/warehouseRouter.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import {
+    getAllWarehouses,
+    postWarehouse,
+    getWarehouse,
+    updateWarehouse,
+    deleteWarehouse,
+    deleteAllWarehouses
+} from '../controllers/warehouseController';
+import asyncHandler from 'express-async-handler';
+import cors from 'cors';
+import { corsOptionsDelegate } from '../utils/cors';
+
+//Declare routes and configure middleware
+const warehouseRouter = express.Router();
+warehouseRouter.use(express.json());
+
+warehouseRouter
+    .route('/')
+    .options(cors(corsOptionsDelegate), (req, res) => {
+        res.sendStatus(200);
+    })
+    .get(cors(), asyncHandler(getAllWarehouses))
+    .post(cors(corsOptionsDelegate), asyncHandler(postWarehouse))
+    .delete(cors(corsOptionsDelegate), asyncHandler(deleteAllWarehouses));
+
+warehouseRouter
+    .route('/:warehouseId')
+    .options(cors(corsOptionsDelegate), (req, res) => {
+        res.sendStatus(200);
+    })
+    .get(cors(), asyncHandler(getWarehouse))
+    .put(cors(corsOptionsDelegate), asyncHandler(updateWarehouse))
+    .delete(cors(corsOptionsDelegate), asyncHandler(deleteWarehouse));
+
+export default warehouseRouter;
