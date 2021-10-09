1ba650d6da80c7529973f8d22bf3ea02562fc9ec
ZeGrandmaster <60135314+danikrisdayadi@users.noreply.github.com>
2021-10-09 22:17:43 +0800

    add montserrat font family

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -3,17 +3,17 @@ import UndrawIcon from '../assets/UndrawIcon.svg'
 
 function LandingPage() {
     return(
-        <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', my: '4rem'}}>
+        <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', my: '4rem', flexWrap: 'wrap'}}>
             <Box component="img" 
             sx={{
-                height: 500,
-                width: 500,
+                height: '30rem',
+                width: '30rem',
             }}
             src={UndrawIcon}/>
             <Box sx={{ display: 'block', margin: '2' }}>
-                <h1>Optimise Your Logistic Flow</h1>
-                <h1>With NextTruck</h1>
-                <p>Increase your Container Freight Station efficiency by reducing LCL cargo through our all-in-one management platform</p>
+                <h1 style={{fontSize: "50px", marginBottom: "10px"}}>Optimise Your Logistic Flow</h1>
+                <h1 style={{fontSize: "80px", marginTop: "0"}}>With NextTruck</h1>
+                <p style={{maxWidth: "40rem"}}>Increase your Container Freight Station efficiency by reducing LCL cargo through our all-in-one management platform</p>
             </Box>
             
         </Box>
