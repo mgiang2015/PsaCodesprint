1ecaaa1d35fe56fe0ca04ffaf93e21beee4c7505
ZeGrandmaster <60135314+danikrisdayadi@users.noreply.github.com>
2021-10-09 21:59:36 +0800

    Add image

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,6 @@ function App() {
                 <LandingPage />
             </Route>
         </Switch>
-        
     </div>
   );
 }
--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -1,8 +1,24 @@
-
+import { Box } from "@mui/system";
+import UndrawIcon from '../assets/UndrawIcon.svg'
 
 function LandingPage() {
     return(
-        <h1>Landing Page</h1>
+        <Box sx={{ display: 'flex' }}>
+            <Box component="img" 
+            sx={{
+                height: 500,
+                width: 500,
+            }}
+            src={UndrawIcon}/>
+            <Box sx={{ display: 'block', margin: '2' }}>
+                <h1>Optimise Your Logistic Flow</h1>
+                <h1>With NextTruck</h1>
+                <p>Increase your Container Freight Station efficiency by reducing LCL cargo through our all-in-one management platform</p>
+            </Box>
+            
+        </Box>
+        
+
     )
 }
 
