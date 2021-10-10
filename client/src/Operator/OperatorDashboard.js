4ad4915cfe50dd1ca7b53d0a4f4860cbe7821e64
mgiang2015 <47556051+mgiang2015@users.noreply.github.com>
2021-10-10 21:00:35 +0800

    Integrate Operator dashboard with backend

--- client/src/Operator/OperatorDashboard.js
+++ client/src/Operator/OperatorDashboard.js
@@ -1,18 +1,44 @@
 import InfoTable from "../Components/InfoTable";
+import { useState, useEffect } from 'react';
+import Datetime from 'react-datetime';
 import OperatorContainer from "./OperatorContainer";
 import { Typography, Box } from '@mui/material';
 
 export default function OperatorDashboard() {
+	const [schedules, setSchedules] = useState([]);
+	const [operators, setOperators] = useState([]);
+
+	useEffect(() => {
+		fetch('/api/schedules')
+			.then(res => res.json())
+			.then(data => {
+				console.log(data);
+				const processedData = data.map(obj => [
+					obj.assignedTruck.licensePlate,
+					(new Date(obj.deliverBy).toLocaleDateString() + " at " + new Date(obj.deliverBy).toLocaleTimeString()),
+					("" + obj.origin.block + "-" + obj.origin.unitNo),
+					("" + obj.destination.block + "-" + obj.destination.unitNo)
+				]);
+				setSchedules(processedData);
+				console.log(schedules);
+			})
+
+		fetch('/api/operators')
+			.then(res => res.json())
+			.then(data => {
+				console.log(data);
+				const processedData = data.map(obj => [
+					obj.username,
+					obj._id,
+					obj.details
+				]);
+				setOperators(processedData);
+				console.log(operators);
+			})
+	}, []);
+
 	const defaultDataset = [
-	["Cell", "Cell", "Cell", "Cell"],
-	["Cell", "Cell", "Cell", "Cell"],
-	["Cell", "Cell", "Cell", "Cell"],
-	["Cell", "Cell", "Cell", "Cell"],
-	["Cell", "Cell", "Cell", "Cell"],
-	["Cell", "Cell", "Cell", "Cell"],
-	["Cell", "Cell", "Cell", "Cell"],
-	["Cell", "Cell", "Cell", "Cell"],
-	["Cell", "Cell", "Cell", "Cell"]
+	["Cell1", "Cell2", "Cell3", "Cell4"]
 	]
 
 	const title1 = "Cargo Schedule";
@@ -24,8 +50,7 @@ export default function OperatorDashboard() {
 	return (
 		<OperatorContainer title={"Welcome back, Operator Dani!"}>
 			<Box sx={{ padding: "4rem" }}>
-				<InfoTable title={title1} headings={headings1} datasets={defaultDataset} />
-				<InfoTable title={title2} headings={headings2} datasets={defaultDataset} />
+				<InfoTable title={title1} headings={headings1} datasets={schedules} />
 			</Box>
 		</OperatorContainer>
 	)
