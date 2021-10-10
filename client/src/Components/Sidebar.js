47a3d6d7fb9afc43f1b909b951b531233c1880f4
ZeGrandmaster <60135314+danikrisdayadi@users.noreply.github.com>
2021-10-10 21:02:08 +0800

    remove commneted lines

--- client/src/Components/Sidebar.js
+++ client/src/Components/Sidebar.js
@@ -5,18 +5,7 @@ import { PaddingY } from '../Utils/Padding';
 const drawerWidth = "15vw";
 
 export default function Sidebar({ options }) {
-	return (
-            // <Box sx={{ color: "#FFFFFF", backgroundColor: "#1D3557", padding: "2vw", height: "100%" }}>
-            //     <MenuList sx={{ height: "100%" }}>
-            //         {options.map((option) => (
-            //             <MenuItem>
-            //                 <Link style={{ textDecoration: "none", color: "#FFFFFF", fontFamily: "Montserrat" }} to={option[1]}>{option[0]}</Link>
-            //                 <PaddingY padding={2}/>
-            //             </MenuItem>
-            //         ))}
-            //     </MenuList>
-            // </Box>
-            
+	return (            
             <Drawer
                 variant="permanent"
                 sx={{
