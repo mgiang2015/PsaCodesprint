e9969c41f94b0eb835e01cb6e9c5f45de85023e0
frisciliasultan <frisciliasultan@gmail.com>
2021-10-10 15:51:30 +0800

    Integrate redux store to app

--- client/src/App.js
+++ client/src/App.js
@@ -1,47 +1,51 @@
-import React from "react";
-import LandingPage from "./pages/LandingPage";
+import React from 'react';
+import LandingPage from './pages/LandingPage';
 import { Redirect, Route, Switch } from 'react-router-dom';
-import LoginPage from "./UserAuth/LoginPage";
-import SignupPage from "./UserAuth/SignupPage";
-import NavBar from "./Components/NavBar";
-import OperatorDashboard from "./Operator/OperatorDashboard";
-import CfsDashboard from "./Cfs/CfsDashboard";
-import CfsOperatorForm from "./Cfs/CfsOperatorForm";
-import OperatorForm from "./Operator/OperatorForm"
-import OperatorTruckForm from "./Operator/OperatorTruckForm"
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import LoginPage from './UserAuth/LoginPage';
+import SignupPage from './UserAuth/SignupPage';
+import NavBar from './Components/NavBar';
+import OperatorDashboard from './Operator/OperatorDashboard';
+import CfsDashboard from './Cfs/CfsDashboard';
+import CfsOperatorForm from './Cfs/CfsOperatorForm';
+import OperatorForm from './Operator/OperatorForm';
+import OperatorTruckForm from './Operator/OperatorTruckForm';
 
 function App() {
-  return (
-    <div>
-        <NavBar />
-        <Switch>
-            <Route exact path="/">
-                <LandingPage />
-            </Route>
-            <Route exact path="/login">
-                <LoginPage />
-            </Route>
-            <Route exact path="/signup">
-                <SignupPage />
-            </Route>
-            <Route exact path="/cfs">
-                <CfsDashboard />
-            </Route>
-            <Route exact path="/cfs/addOperator">
-                <CfsOperatorForm />
-            </Route>
-            <Route exact path="/operator">
-                <OperatorDashboard />
-            </Route>
-            <Route exact path="/operator/addDelivery">
-                <OperatorForm />
-            </Route>
-            <Route exact path="/operator/addTruck">
-                <OperatorTruckForm />
-            </Route>
-        </Switch>
-    </div>
-  );
+    return (
+        <Provider store={store}>
+            <div>
+                <NavBar />
+                <Switch>
+                    <Route exact path="/">
+                        <LandingPage />
+                    </Route>
+                    <Route exact path="/login">
+                        <LoginPage />
+                    </Route>
+                    <Route exact path="/signup">
+                        <SignupPage />
+                    </Route>
+                    <Route exact path="/cfs">
+                        <CfsDashboard />
+                    </Route>
+                    <Route exact path="/cfs/addOperator">
+                        <CfsOperatorForm />
+                    </Route>
+                    <Route exact path="/operator">
+                        <OperatorDashboard />
+                    </Route>
+                    <Route exact path="/operator/addDelivery">
+                        <OperatorForm />
+                    </Route>
+                    <Route exact path="/operator/addTruck">
+                        <OperatorTruckForm />
+                    </Route>
+                </Switch>
+            </div>
+        </Provider>
+    );
 }
 
 export default App;
