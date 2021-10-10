81c35c7f0cac9170ebf6db4a59308481c8d2b2d3
frisciliasultan <frisciliasultan@gmail.com>
2021-10-10 18:00:31 +0800

    Halfway signup login

--- client/src/UserAuth/LoginPage.js
+++ client/src/UserAuth/LoginPage.js
@@ -1,39 +1,100 @@
-import React, {useState} from "react";
-import { useForm } from "react-hook-form";
-import { Paper, TextField, Button, Container, Box, Divider, FormControl, InputLabel, Select, MenuItem } from '@mui/material';
-import { Logo } from "../assets/exportLogo";
-import { PaddingY } from "../Utils/Padding";
-export default function LoginPage() {
-	const { register, handleSubmit } = useForm();
-	const onSubmit = function(data) {
-		// specify action later
-        data["userType"] = userType;
-		console.log(data);
-	}
+import React, { useState } from 'react';
+import { withRouter } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { connect } from 'react-redux';
+import { loginUser } from '../redux/actions/authActions';
+import {
+    Paper,
+    TextField,
+    Button,
+    Container,
+    Box,
+    Divider,
+    FormControl,
+    InputLabel,
+    Select,
+    MenuItem
+} from '@mui/material';
+import { Logo } from '../assets/exportLogo';
+import { PaddingY } from '../Utils/Padding';
 
-    const [userType, setUserType] = useState("");
-    const handleChange = function(data) {
+function LoginPage(props) {
+    const [userType, setUserType] = useState('operators');
+    const [userInput, setUserInput] = useState({
+        username: '',
+        password: '',
+        errors: {
+            email: '',
+            password: '',
+            message: ''
+        }
+    });
+
+    const { register, handleSubmit } = useForm();
+    const onSubmit = function (data) {
+        setUserInput({
+            ...userInput,
+            username: data['username'],
+            password: data['password']
+        });
+
+        // specify action later
+        // data['userType'] = userType;
+        console.log(data);
+        props.loginUser(data, props.history, userType);
+    };
+
+    const handleChange = function (data) {
         setUserType(data.target.value);
-    }
+    };
 
-	return (
-        <Container sx={{height: "80vh", width: "100%", alignItems:"center", justifyContent: "center", display: "flex"}}>
+    return (
+        <Container
+            sx={{
+                height: '80vh',
+                width: '100%',
+                alignItems: 'center',
+                justifyContent: 'center',
+                display: 'flex'
+            }}
+        >
             <form onSubmit={handleSubmit(onSubmit)}>
-                <Paper elevation={12}
-                    sx={{ display: 'flex',
+                <Paper
+                    elevation={12}
+                    sx={{
+                        display: 'flex',
                         flexDirection: 'column',
                         alignItems: 'center',
                         px: 6,
                         py: 6,
                         borderRadius: 2
-                    }}>
-                    <Logo height={100}/>
+                    }}
+                >
+                    <Logo height={100} />
                     <h1>Welcome Back!</h1>
-                    <Box sx={{flexDirection: 'column',display: 'flex', gap: 2, width: "40vh"}}>
-                        <TextField label="Username" variant="outlined" {...register("Username")} />
-                        <TextField label="Password" variant="outlined" type="password" {...register("Password")} />
-                        <FormControl fullWidth variant="standard" >
-                            <InputLabel id="demo-simple-select-label">User Type </InputLabel>
+                    <Box
+                        sx={{
+                            flexDirection: 'column',
+                            display: 'flex',
+                            gap: 2,
+                            width: '40vh'
+                        }}
+                    >
+                        <TextField
+                            label="Username"
+                            variant="outlined"
+                            {...register('username')}
+                        />
+                        <TextField
+                            label="Password"
+                            variant="outlined"
+                            type="password"
+                            {...register('password')}
+                        />
+                        <FormControl fullWidth variant="standard">
+                            <InputLabel id="demo-simple-select-label">
+                                User Type{' '}
+                            </InputLabel>
                             <Select
                                 labelId="demo-simple-select-label"
                                 id="demo-simple-select"
@@ -41,24 +102,62 @@ export default function LoginPage() {
                                 label="User Type"
                                 onChange={handleChange}
                             >
-                                <MenuItem value={"operators"}>Operator</MenuItem>
-                                <MenuItem value={"cfsAdmins"}>CFS Admin</MenuItem>
+                                <MenuItem value={'operators'}>
+                                    Operator
+                                </MenuItem>
+                                <MenuItem value={'cfsAdmins'}>
+                                    CFS Admin
+                                </MenuItem>
                             </Select>
                         </FormControl>
-
                     </Box>
                     <PaddingY padding={1.5} />
-                    <Box sx={{display: 'flex', justifyContent: 'flex-end', width: "100%"}}>
-                        <Button size="large" variant="contained" sx={{display: 'flex', px: 5, textTransform: 'none'}} type="submit">Login</Button>
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            justifyContent: 'flex-end',
+                            width: '100%'
+                        }}
+                    >
+                        <Button
+                            size="large"
+                            variant="contained"
+                            sx={{
+                                display: 'flex',
+                                px: 5,
+                                textTransform: 'none'
+                            }}
+                            type="submit"
+                        >
+                            Login
+                        </Button>
                     </Box>
                     <PaddingY padding={2} />
 
-                    <Divider style={{width:'100%'}}>or</Divider>
+                    <Divider style={{ width: '100%' }}>or</Divider>
 
                     <PaddingY padding={2} />
-                    <Button variant="contained" sx={{display: 'flex', px: 5, textTransform: 'none', width: '100%'}} href="/signup">Sign Up</Button>
+                    <Button
+                        variant="contained"
+                        sx={{
+                            display: 'flex',
+                            px: 5,
+                            textTransform: 'none',
+                            width: '100%'
+                        }}
+                        href="/signup"
+                    >
+                        Sign Up
+                    </Button>
                 </Paper>
             </form>
         </Container>
-	)
+    );
 }
+
+const mapStateToProps = (state) => ({
+    auth: state.auth,
+    error: state.errors
+});
+
+export default connect(mapStateToProps, { loginUser })(withRouter(LoginPage));
--- client/src/UserAuth/SignupPage.js
+++ client/src/UserAuth/SignupPage.js
@@ -1,39 +1,90 @@
-import React, {useState} from "react";
-import { useForm } from "react-hook-form";
-import { Paper, TextField, Button, Container, Box, Divider, FormControl, InputLabel, Select, MenuItem } from '@mui/material';
-import { Logo } from "../assets/exportLogo";
-import { PaddingY } from "../Utils/Padding";
+import React, { useState } from 'react';
+import { withRouter } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { connect } from 'react-redux';
+import { registerUser } from '../redux/actions/authActions';
+import {
+    Paper,
+    TextField,
+    Button,
+    Container,
+    Box,
+    Divider,
+    FormControl,
+    InputLabel,
+    Select,
+    MenuItem
+} from '@mui/material';
+import { Logo } from '../assets/exportLogo';
+import { PaddingY } from '../Utils/Padding';
 
-export default function SignupPage() {
-    const [userType, setUserType] = useState("");
-	const { register, handleSubmit } = useForm();
-	const onSubmit = function(data) {
-        data["userType"] = userType;
-	}
+function SignupPage() {
+    const [userType, setUserType] = useState('operators');
 
-    const handleChange = function(data) {
+    const { register, handleSubmit } = useForm();
+
+    const onSubmit = function (data) {
+        data['userType'] = userType;
+        console.log(data);
+    };
+
+    const handleChange = function (data) {
         setUserType(data.target.value);
-    }
+    };
 
-	return (
-        <Container sx={{maxHeight: "90vh", width: "100%", alignItems:"center", justifyContent: "center", display: "flex"}}>
+    return (
+        <Container
+            sx={{
+                maxHeight: '90vh',
+                width: '100%',
+                alignItems: 'center',
+                justifyContent: 'center',
+                display: 'flex'
+            }}
+        >
             <form onSubmit={handleSubmit(onSubmit)}>
-                <Paper elevation={12}
-                    sx={{ display: 'flex',
+                <Paper
+                    elevation={12}
+                    sx={{
+                        display: 'flex',
                         flexDirection: 'column',
                         alignItems: 'center',
                         px: 6,
                         py: 6,
                         borderRadius: 2
-                    }}>
-                    <Logo height={100}/>
+                    }}
+                >
+                    <Logo height={100} />
                     <h1>Create New Account</h1>
-                    <Box sx={{flexDirection: 'column', display: 'flex', gap: 2, width: "40vh"}}>
-                        <TextField label="Username" variant="outlined" {...register("username")} />
-                        <TextField label="Password" variant="outlined" type="password"  {...register("password")}/>
-                        <TextField label="Confirm Password" variant="outlined" type="password" {...register("password2")}/>
-                        <FormControl fullWidth variant="standard" >
-                            <InputLabel id="demo-simple-select-label">User Type </InputLabel>
+                    <Box
+                        sx={{
+                            flexDirection: 'column',
+                            display: 'flex',
+                            gap: 2,
+                            width: '40vh'
+                        }}
+                    >
+                        <TextField
+                            label="Username"
+                            variant="outlined"
+                            {...register('username')}
+                        />
+                        <TextField
+                            label="Password"
+                            variant="outlined"
+                            type="password"
+                            {...register('password')}
+                        />
+                        <TextField
+                            label="Confirm Password"
+                            variant="outlined"
+                            type="password"
+                            {...register('password2')}
+                        />
+                        <FormControl fullWidth variant="standard">
+                            <InputLabel id="demo-simple-select-label">
+                                User Type{' '}
+                            </InputLabel>
                             <Select
                                 labelId="demo-simple-select-label"
                                 id="demo-simple-select"
@@ -41,21 +92,62 @@ export default function SignupPage() {
                                 label="User Type"
                                 onChange={handleChange}
                             >
-                                <MenuItem value={"operators"}>Operator</MenuItem>
-                                <MenuItem value={"cfsAdmins"}>CFS Admin</MenuItem>
+                                <MenuItem value={'operators'}>
+                                    Operator
+                                </MenuItem>
+                                <MenuItem value={'cfsAdmins'}>
+                                    CFS Admin
+                                </MenuItem>
                             </Select>
                         </FormControl>
                     </Box>
                     <PaddingY padding={1.5} />
-                    <Box sx={{display: 'flex', justifyContent: 'flex-end', width: "100%"}}>
-                        <Button size="large" variant="contained" sx={{display: 'flex', px: 5, textTransform: 'none'}} type="submit">Sign Up</Button>
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            justifyContent: 'flex-end',
+                            width: '100%'
+                        }}
+                    >
+                        <Button
+                            size="large"
+                            variant="contained"
+                            sx={{
+                                display: 'flex',
+                                px: 5,
+                                textTransform: 'none'
+                            }}
+                            type="submit"
+                        >
+                            Sign Up
+                        </Button>
                     </Box>
                     <PaddingY padding={2} />
-                    <Divider style={{width:'100%'}}>or</Divider>
+                    <Divider style={{ width: '100%' }}>or</Divider>
                     <PaddingY padding={2} />
-                    <Button variant="contained" sx={{display: 'flex', px: 5, textTransform: 'none', width: '100%'}} href="/login">Login</Button>
+                    <Button
+                        variant="contained"
+                        sx={{
+                            display: 'flex',
+                            px: 5,
+                            textTransform: 'none',
+                            width: '100%'
+                        }}
+                        href="/login"
+                    >
+                        Login
+                    </Button>
                 </Paper>
             </form>
         </Container>
-	)
+    );
 }
+
+const mapStateToProps = (state) => ({
+    auth: state.auth,
+    error: state.errors
+});
+
+export default connect(mapStateToProps, { registerUser })(
+    withRouter(SignupPage)
+);
